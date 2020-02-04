72baf999aef9f9f4addd76b65b45f9ebd57967ba
njsokol <njsokol@us.ibm.com>
2020-02-03 20:44:46 -0600

    Update: don't use src folder

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 const PORT = 12619;
 
 // Host static assets
-app.use("/", express.static(`src`));
+app.use("/", express.static(`styles`));
 
 // Serve it up
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/src/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(PORT, () => console.log(`Running on ${PORT}`));
